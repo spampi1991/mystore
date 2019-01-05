63c27d7d14d55372e869d58c7eca59a13ef83d53
Francesco <spampi1991@gmail.com>
2019-01-05 16:14:59 +0100

    Aggiunto Capistrano

--- Gemfile
+++ Gemfile
@@ -5,10 +5,9 @@ ruby '2.6.0'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.2'
-# Use sqlite3 as the database for Active Record
-gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
+gem 'sqlite3'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -52,6 +51,7 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'capistrano', '~> 2.0'
 end
 
 group :test do
@@ -60,6 +60,10 @@ group :test do
   gem 'selenium-webdriver'
   # Easy installation and use of chromedriver to run system tests with Chrome
   gem 'chromedriver-helper'
+end
+
+group :production do
+  gem 'pg' 
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
---
+++ config/deploy.rb
@@ -0,0 +1,35 @@
+require "bundler/capistrano"
+load "deploy/assets"
+
+set :application, "mystore"
+set :repository,  "https://github.com/spampi1991/mystore.git"
+set :scm, :git 
+server = "ec2-18-206-226-204.compute-1.amazonaws.com"
+
+role :web, server                          # Your HTTP server, Apache/etc
+role :app, server                          # This may be the same as your `Web` server
+role :db, server, :primary => true         # This is where Rails migrations will run
+
+set :user, "spree"
+
+set :deploy_to, "/home/spree/#{application}"
+set :use_sudo, false
+
+ssh_options[:keys] = %w(/home/francesco/i-024922b078ed8a292.pem)
+default_run_options[:shell] = '/bin/bash --login'
+default_environment['RAILS_ENV'] = 'production'
+
+# if you want to clean up old releases on each deploy uncomment this:
+# after "deploy:restart", "deploy:cleanup"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
